bc02ed32cd72ebb9b904a5de9dea9d1fafdef3f3
desilaupretre <desulisfr@gmail.com>
2021-06-01 23:36:23 +0200

    Vue.js installed

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/icon-above-font.png">
+    <Groupomania msg="Groupomania"/>
+  </div>
+</template>
+
+<script>
+import Groupomania from './components/Groupomania.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Groupomania
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
