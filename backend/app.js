0333bc142d067a84aaa2ce719f3c124d48970da0
desilaupretre <desulisfr@gmail.com>
2021-06-02 00:02:46 +0200

    MySQL.js installed

--- backend/app.js
+++ backend/app.js
@@ -8,20 +8,30 @@ const cors = require('cors');
 
 // const bodyParser = require('body-parser'); //import body-parser
 
-const mongoose = require('mongoose'); //import mongoose
+const mysql = require('mysql'); //import mongoose
 
-const userRoutes = require('./routes/user'); //import router for user route
+// const userRoutes = require('./routes/user'); //import router for user route
 
-const postRoutes = require('./routes/post'); 
+// const postRoutes = require('./routes/post'); 
 
-const commentRoutes = require('./routes/comment');
+// const commentRoutes = require('./routes/comment');
 
-mongoose.connect('mongodb+srv://' + process.env.DB_USER + ':'+ process.env.DB_PASS + '@' + process.env.DB_HOST + '/' + process.env.DB_NAME + '?retryWrites=true&w=majority',
-{ useNewUrlParser: true,
-  useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch((e) => console.log('Connexion à MongoDB échouée !', e));
-  
+var connection = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME,
+});
+connection.connect();
+
+connection.query('SELECT * from groupomania.user', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0]);
+});
+
+connection.end();
+
+
 const app = express(); 
 
 const path = require('path'); //path to server
@@ -33,9 +43,9 @@ app.use(helmet()); //security configuration to safeguard application or API from
 app.use(express.json());
 // app.use(bodyParser.json()); //define json as a middleware global function for app
 
-app.use('/api/user', userRoutes); //save as a unique route to all request on /api/stuff
-app.use('/api/post', postRoutes); //save for the user authentification
-app.use('/api/comment', commentRoutes);
+// app.use('/api/user', userRoutes); //save as a unique route to all request on /api/stuff
+// app.use('/api/post', postRoutes); //save for the user authentification
+// app.use('/api/comment', commentRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images'))); //indicate to express to manage 'images' in static subrepo __dirname
 
 module.exports = app;
