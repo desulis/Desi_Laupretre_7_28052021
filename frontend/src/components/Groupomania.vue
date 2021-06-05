26155b1683a40f2f831823c151364e45938912f3
desilaupretre <desulisfr@gmail.com>
2021-06-05 18:37:07 +0200

    Start with Vue.js.
    Some implementation router.js

--- frontend/src/components/Groupomania.vue
+++ frontend/src/components/Groupomania.vue
@@ -1,56 +1,36 @@
+<script>
+	export default {
+		name: 'Groupomania',
+		props: ['url', 'text'],
+        beforeCreate() { console.log('Je ne suis pas encore  créé') },
+        created() { console.log('Je suis créé !') },
+        beforeMount() { console.log('Je vais bientôt être monté sur le DOM!') },
+        mounted() { console.log('Je suis monté sur le DOM!') },
+        beforeDestroy() { console.log('Je suis sur le point de disparaître du DOM !') },
+        destroyed() { console.log('Je suis supprimé') }
+	}
+</script>
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      
-    </p>
-    <h3></h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<a :href="url">{{ text }}</a>
 </template>
 
-<script>
-export default {
-  name: 'Groupomania',
-  props: {
-    msg: String
-  }
-}
-</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+	h3 {
+		margin: 40px 0 0;
+	}
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+	a {
+		color: #42b983;
+	}
+	
 </style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="logo" src="../assets/icon-above-font.png">
+    <Groupomania msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Groupomania from '@/components/Groupomania.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Groupomania
+  }
+}
+</script>
